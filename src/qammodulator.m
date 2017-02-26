a0e947174ff16f0b0ebdb50c22f8f647076fcffb
Steven Cantrell <scantrell7accts@gmail.com>
2017-02-25 23:52:05 -0500

    Implemented QAM modulator tests.

--- src/qammodulator.m
+++ src/qammodulator.m
@@ -6,13 +6,26 @@
     %   Date Created: 2/25/2017
     %   Version: 1
     %       (2/25/2017) Initial commit.
+    %   Version: 2
+    %       (2/25/2017) Implemented Modulate method.
 
 classdef qammodulator < qam & modulator
 %% Public Methods
     methods
         % Modulates a bit stream into mapped symbols
-        function symbols = Modulate(obj, bitstream)
-            error('qammodulator.Modulate not implemented');
+        function symbols = Modulate(obj, bitstream)          
+            [m, n] = size(bitstream);
+            if (m == 1 || n == 1)
+                bitstream = bitstream';
+            else
+                error('Bitstream must be a row or column vector');
+            end
+            if (mod(length(bitstream), log2(obj.M)) ~= 0)
+                error('Bitstream length must be divisible by log2(M)');
+            end
+            
+            mappings = bi2de(reshape(bitstream, [log2(obj.M), numel(bitstream) / log2(obj.M)])', 'left-msb');
+            symbols = obj.Constellation(obj.SymbolMapping(mappings + 1) + 1); % Mappings incremented to index at start of 1
         end
     end
 %% Public Methods
--- tests/qammodulatorTest.m
+++ tests/qammodulatorTest.m
@@ -6,15 +6,19 @@
     %   Date Created: 2/25/2017
     %   Version: 1
     %       (2/25/2017) Initial commit.
+    %   Version: 2
+    %       (2/25/2017) Implemented and tested Modulate test method.
 
 classdef qammodulatorTest < matlab.unittest.TestCase
 %% Test Methods
     methods (Test)
         function ModulateTest(testCase)
             modulator = qammodulator();
+            modulator.M = 4;
+            modulator.SymbolOrder = 'Binary';
             bitstream = [0 1 0 0 1 0 1 1];
             actSolution = modulator.Modulate(bitstream);
-            expSolution = [1+1i -1+1i -1-1i 1-1i];
+            expSolution = modulator.Constellation([2 1 3 4]);
             testCase.verifyEqual(actSolution, expSolution);
         end
     end
