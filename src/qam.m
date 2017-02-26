8f68741312363582811b2a5bb1886172ee745f3b
Steven Cantrell <scantrell7accts@gmail.com>
2017-02-25 23:52:05 -0500

    Implemented QAM modulator tests.

--- src/qam.m
+++ src/qam.m
@@ -6,23 +6,26 @@
     %   Date Created: 2/25/2017
     %   Version: 1
     %       (2/25/2017) Initial commit.
+    %   Version: 2
+    %       (2/25/2017) Corrected property attributes. Added symbol
+    %       mapping. Moved GetConstellation to property. Removed class
+    %       abstract attribute.
     
-classdef (Abstract) qam
+classdef qam
 %% Properties
     properties(Constant, Access=private)
         dM = 4                       % Default alphabet size
-    end
-    
-    properties(Access=private)
-        pM                           % Symbol alphabet size
+        dSymbolOrder = 'Binary'      % Default symbol order
     end
     
     properties(SetAccess=private)
         Constellation = []           % Symbol alphabet
+        SymbolMapping = []           % Symbol to bit mapping
     end
     
-    properties(Dependent)
+    properties
         M                            % Symbol alphabet size
+        SymbolOrder                  % Order to determine symbol mapping
     end
 %% Properties
     
@@ -31,11 +34,12 @@ classdef (Abstract) qam
         % Constructor
         function obj = qam()
             obj.M = obj.dM;
+            obj.SymbolOrder = obj.dSymbolOrder;
         end
         
         % M Get Accessor
         function M = get.M(obj)
-            M = obj.pM;
+            M = obj.M;
         end
         
         % M Set Accessor
@@ -43,22 +47,40 @@ classdef (Abstract) qam
             if mod(log2(val), 2) ~= 0
                 error('log2(M) must be even')
             end
-            obj.pM = val;
-            obj.Constellation = obj.GetConstellation(obj.pM);
+            obj.M = val;
+        end
+        
+        % SymbolOrder Get Accessor
+        function SymbolOrder = get.SymbolOrder(obj)
+            SymbolOrder = obj.SymbolOrder;
+        end
+        
+        % SymbolOrder Set Accessor
+        function obj = set.SymbolOrder(obj, val)
+            switch lower(val)
+                case {'binary'}
+                    obj.SymbolOrder = val;
+                otherwise
+                    error('Incorrect SymbolOrder. Choose from: Binary');
+            end
+        end
+        
+        % Constellation Get Accessor
+        function constellation = get.Constellation(obj)
+            pamConstellation = [-log2(obj.M)+1:2:-1, 1:2:log2(obj.M)];
+            constellationMat = repmat(pamConstellation, length(pamConstellation), 1) + 1i*repmat(flip(pamConstellation)', 1, length(pamConstellation));
+            constellation = reshape(constellationMat, [1, numel(constellationMat)]);
+        end
+        
+        % SymbolMapping Get Accessor
+        function mapping = get.SymbolMapping(obj)
+            switch lower(obj.SymbolOrder)
+                case {'binary'}
+                    mapping = 0:obj.M-1;
+                otherwise
+                    error('Incorrect SymbolOrder. Set SymbolOrder to: Binary');
+            end
         end
     end
 %% Public Methods
-
-%% Static Methods
-    methods(Static)
-        % Creates QAM alphabet
-        function constellation = GetConstellation(M)
-            if mod(log2(M), 2) ~= 0
-                error('log2(M) must be even')
-            end
-            pamConstellation = [-1*log2(M)-1:2:-1, 1:2:1*log2(M)+1];
-            constellation = repmat(pamConstellation, length(pamConstellation), 1) + 1i*repmat(flip(pamConstellation)', 1, length(pamConstellation));
-        end
-    end
-%% Static Methods
 end
--- tests/runtests.m
+++ tests/runtests.m
@@ -6,11 +6,18 @@
     %   Date Created: 2/25/2017
     %   Version: 1
     %       (2/25/2017) Initial commit.
+    %   Version: 2
+    %       (2/25/2017) Added QAM base class test. Fixed qamdemodulator
+    %       test call.
+
+%% Execute qam Tests
+qamTests = matlab.unittest.TestSuite.fromClass(?qamTest);
+qamResult = run(qamTests);
 
 %% Execute qammodulator Tests
 qammodulatorTests = matlab.unittest.TestSuite.fromClass(?qammodulatorTest);
 qammodulatorResult = run(qammodulatorTests);
 
 %% Execute qamdemodulator Tests
-qamdemodulatorTests = matlab.unittest.TestSuite.fromClass(?qammodulatorTest);
+qamdemodulatorTests = matlab.unittest.TestSuite.fromClass(?qamdemodulatorTest);
 qamdemodulatorResult = run(qamdemodulatorTests);
