349f8ac0648dcaccbc39f223cbfc7f3951e635f2
Steven Cantrell <scantrell7accts@gmail.com>
2017-03-12 03:16:38 -0400

    Integrated findi into test

--- src/+comms/findi.m
+++ src/+comms/findi.m
@@ -1,9 +1,12 @@
+%FINDI
     %   Description: Gets the indicies of the elements in a match vector
     %   found in a random vector containing the match elements.
     
 %VERSIONING
     %   Author: Steven Cantrell
+    %   Date Created: 3/12/2017
     %   Changelog:
+    %     (3/12/2017) Initial commit
 
 function y = findi(match, x)
     y = mod(find((match == x)')', numel(match));
